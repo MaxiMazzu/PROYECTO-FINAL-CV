f730a498ce06262800365aa5d7f8877172147c18
MaxiMazzu <maxi-m-17@hotmail.com>
2023-01-27 18:02:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('boton').addEventListener('click', ()=>{
+    document.getElementById('boton').style.background = '#D29BFD';
+   });
+
+   document.getElementById('botonDos').addEventListener('click', ()=>{
+    document.getElementById('botonDos').style.background = '#D29BFD';
+   });
+   
